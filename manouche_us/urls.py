a694daae86d40e265904d0c59bf341956c5cf9f2
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-30 11:02:07 -0300

    Add routes to index

--- manouche_us/urls.py
+++ manouche_us/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r"^$", "django.views.generic.simple.direct_to_template", {"template": "index.html"}),
+    url(r"^$", include('project.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
