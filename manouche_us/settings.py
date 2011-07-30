0c8bc1d0b676063e3c1549baeeea88ccbe009f50
Francisco Souza <francisco@franciscosouza.net>
2011-07-30 12:48:59 -0300

    Settings for proper serve statics

--- manouche_us/settings.py
+++ manouche_us/settings.py
@@ -58,8 +58,7 @@ MEDIA_URL = 'media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATICFILES_DIRS = ('static/',)
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
