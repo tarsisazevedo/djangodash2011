7bf727550c359b358c87c2efef32867b2fee7492
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-30 00:58:43 -0300

    Add extract function. Add source field

--- manouche_us/project/models.py
+++ manouche_us/project/models.py
@@ -1,4 +1,8 @@
+import tarfile
+import uuid
+
 from django.db import models
+from django.conf import settings
 from django.core.files import File
 
 
@@ -7,6 +11,7 @@ class ProjectUrlException(Exception):
 
 class Project(models.Model):
     url = models.URLField()
+    source = models.CharField(max_length=200)
 
     def validate_url(self):
         return "github" in self.url
@@ -15,3 +20,11 @@ class Project(models.Model):
         if not self.validate_url():
             raise ProjectUrlException()
         return File(self.url)
+
+    def extract_code(self):
+        source_tar = self.get_source()
+        source = tarfile.open(source_tar.file)
+
+        self.source = settings.MEDIA_URL + "sources/extracted/%s/" % uuid.uuid4()
+        source.extractall(path=self.source)
+        self.save()
