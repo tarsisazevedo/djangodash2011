f8a2f8d1516b403ec5544158eecb91af8ef686d3
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-30 17:04:41 -0300

    Add wait route

--- manouche_us/project/urls.py
+++ manouche_us/project/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
+from django.views.generic import TemplateView
 
 urlpatterns = patterns("project.views",
                         url(r"^$", 'index', name="index"),
+                        url(r"^wait/$", TemplateView.as_view(template_name="projects/wait.html"), name="wait"),
                       )
