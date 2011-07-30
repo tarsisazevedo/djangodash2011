03a331793827213188a3837214d13f92a72ba1ad
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-30 17:07:17 -0300

    Add new route to wait page

--- manouche_us/urls.py
+++ manouche_us/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls.defaults import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-
+from django.views.generic import TemplateView
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r"^$", include('project.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r"^wait/$", TemplateView.as_view(template_name="project/wait.html"), name="wait"),
 )
 
 urlpatterns += staticfiles_urlpatterns()
