9ca280a49ad62ca90840f18dc72172005b2fd14f
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-29 23:17:05 -0300

    Add tests to support download of source and validate github url

--- manouche_us/project/tests/test_models.py
+++ manouche_us/project/tests/test_models.py
@@ -1,7 +1,23 @@
-from django.util import unittest
+import mimetypes
+
+from django.conf import settings
+from django.utils import unittest
+
 from project.models import Project
+
 
 class TestModels(unittest.TestCase):
     def test_create_a_project(self):
         project = Project.objects.create(url="foo")
         self.assertTrue(project)
+
+    def test_url_should_be_from_github(self):
+        project = Project.objects.create(url="github.com")
+        self.assertTrue(project.validate_url())
+
+    def test_get_source_of_project(self):
+        project = Project.objects.create(url="media/sources/fake.tar.gz")
+        source = project.get_source()
+
+        self.assertEquals(source.file, settings.MEDIA_URL + "sources/fake.tar.gz")
+        self.assertEquals(mimetypes.guess_type(source.file), ('application/x-tar', 'gzip'))
