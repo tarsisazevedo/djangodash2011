0e05c0287c126ece56253f0d16a65b83a3f4f914
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-29 23:17:58 -0300

    Add suport to download source and validade link:

--- manouche_us/project/models.py
+++ manouche_us/project/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.files import File
 
-# Create your models here.
+class Project(models.Model):
+    url = models.URLField()
+
+    def validate_url(self):
+        return "github" in self.url
+
+    def get_source(self):
+        return File(self.url)
