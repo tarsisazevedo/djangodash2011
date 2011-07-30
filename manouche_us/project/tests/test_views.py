45ef0655d81a42a4cf3b0c3587b68601e6d06c4c
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-30 11:32:27 -0300

    Add tests to form validation

--- manouche_us/project/tests/test_views.py
+++ manouche_us/project/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.utils import unittest
-from django.test import RequestFactory
+from django.test import RequestFactory, Client
 
 from project.views import index
 
@@ -11,3 +11,12 @@ class TestViews(unittest.TestCase):
         response = index(request)
 
         self.assertEquals(response.context_data.keys(), ['form'])
+
+    def test_error_when_url_is_not_github(self):
+        url = "http://foo.com"
+        client = Client()
+        response = client.post("/", {"url": url})
+
+        form_erros = response.context[0].get('form').errors
+        self.assertEquals(form_erros, {'url': [u"Url should be from github.com! I\'m a fanboy :D"]})
+
