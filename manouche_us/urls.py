7f23d91a2e620c9eca8362003becf3ffc0f3faa9
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-30 01:41:13 -0300

    Add url to index

--- manouche_us/urls.py
+++ manouche_us/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r"^$", "django.views.generic.simple.direct_to_template", {"template": "index.html"}),
     url(r'^admin/', include(admin.site.urls)),
 )
