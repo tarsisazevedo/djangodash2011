78b627a4849a2de009aadeac26aa44c135abe734
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-31 04:47:47 -0300

    Add tests to new analyzer

--- manouche_us/project/tests/test_analyzers.py
+++ manouche_us/project/tests/test_analyzers.py
@@ -1,7 +1,7 @@
 from django.utils import unittest
 
 from project.models import Project
-from project.analyzer import BaseAnalyzer
+from project.analyzer import BaseAnalyzer, CoverageAnalyzer, ClonneDiggerAnalyzer
 
 class BaseAnalyzerTestCase(unittest.TestCase):
     def setUp(self):
@@ -11,8 +11,25 @@ class BaseAnalyzerTestCase(unittest.TestCase):
     def test_base_analyzer_get_project_modules_should_return_a_list_of_all_python_module_inside_its(self):
         python_modules = self.analyzer.get_project_modules()
 
-        self.assertEquals(python_modules, ['manouche_us'])
+        self.assertEquals(python_modules, ['apps', 'site_media', 'templates'])
         self.analyzer._remove_extracted_code()
 
     def test_base_analyzer_analyze_should_raises_not_implemented_error(self):
         self.assertRaises(NotImplementedError, self.analyzer.analyze)
+
+class CoverageAnalyzerTest(unittest.TestCase):
+    def setUp(self):
+        self.project = Project.objects.create(url="media/sources/fake-github.tar.gz")
+        self.analyzer = CoverageAnalyzer(self.project.id)
+
+    @unittest.skip
+    def test_get_coverage_from_project(self):
+        coverage = self.analyzer.analyze()
+        self.assertEquals(17, coverage)
+
+class ClonneDiggerTest(unittest.TestCase):
+    def test_get_clonnedigger_report_from_project(self):
+        project = Project.objects.create(url="media/sources/fake-github.tar.gz")
+        analyzer = ClonneDiggerAnalyzer(project.id)
+
+        self.assertAlmostEquals(19.24, analyzer.analyze())
