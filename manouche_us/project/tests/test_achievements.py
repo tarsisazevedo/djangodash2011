ef96da32977c92613f0f3918a461130fba6ea274
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-31 13:36:40 -0300

    Add test to newbie achievement

--- manouche_us/project/tests/test_achievements.py
+++ manouche_us/project/tests/test_achievements.py
@@ -1,6 +1,7 @@
 from django.utils import unittest
+from django.conf import settings
 
-from project.achievements import PEP8Achievement, FakePythonist
+from project.achievements import PEP8Achievement, FakePythonist, NewbiePythonist
 
 class TestPEP8Achievement(unittest.TestCase):
     def test_fake_pythonist_achievement(self):
@@ -9,4 +10,12 @@ class TestPEP8Achievement(unittest.TestCase):
 
         self.assertTrue(isinstance( pep8_achievement, FakePythonist ))
         self.assertEquals(pep8_achievement.name, "Fake Pythonist")
-        self.assertEquals(pep8_achievement.image, 'static/img/fake_pythonist.png')
+        self.assertEquals(pep8_achievement.image, settings.STATIC_ROOT + '/img/fake_pythonist.png')
+
+    def test_newbie_pythonist_achievement(self):
+        pep8_result = 101
+        pep8_achievement = PEP8Achievement(pep8_result).get_achievement()
+
+        self.assertTrue(isinstance( pep8_achievement, NewbiePythonist ))
+        self.assertEquals(pep8_achievement.name, "Newbie Pythonist")
+        self.assertEquals(pep8_achievement.image, settings.STATIC_ROOT + '/img/newbie_pythonist.png')
