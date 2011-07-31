0db272cbf7b721f3e64e56a94f62cd579c240113
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-31 13:48:41 -0300

    Add tests to guido achievement

--- manouche_us/project/tests/test_achievements.py
+++ manouche_us/project/tests/test_achievements.py
@@ -1,7 +1,7 @@
 from django.utils import unittest
 from django.conf import settings
 
-from project.achievements import PEP8Achievement, FakePythonist, NewbiePythonist, SeniorPythonist, YouArePythonic
+from project.achievements import PEP8Achievement, FakePythonist, NewbiePythonist, SeniorPythonist, YouArePythonic, HelloGuido
 
 class TestPEP8Achievement(unittest.TestCase):
     def test_fake_pythonist_achievement(self):
@@ -35,3 +35,11 @@ class TestPEP8Achievement(unittest.TestCase):
         self.assertTrue(isinstance( pep8_achievement, YouArePythonic ))
         self.assertEquals(pep8_achievement.name, "You are pythonic")
         self.assertEquals(pep8_achievement.image, settings.STATIC_ROOT + '/img/u_are_pythonic.png')
+
+    def test_guido_achievement(self):
+        pep8_result = 0
+        pep8_achievement = PEP8Achievement(pep8_result).get_achievement()
+
+        self.assertTrue(isinstance( pep8_achievement, HelloGuido ))
+        self.assertEquals(pep8_achievement.name, "Hello Guido")
+        self.assertEquals(pep8_achievement.image, settings.STATIC_ROOT + '/img/guido.png')
