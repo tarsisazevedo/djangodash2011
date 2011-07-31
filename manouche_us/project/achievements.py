2f95046589770dd5dfa89ff6f8bf695eee5a23a9
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-07-31 13:21:20 -0300

    Add achievements file

---
+++ manouche_us/project/achievements.py
@@ -0,0 +1,36 @@
+class PEP8Achievement(object):
+    FAKE_PYTHONIST = 300
+    NEWBIE_PYTHONIST = 100
+    SENIOR_PYTHONIST = 30
+    U_ARE_PYTHONIC = 15
+    GUIDO = 0
+
+    def __init__(self, result_pep8):
+        self.result = result_pep8
+
+    def get_achievement(self):
+        if self.result > self.FAKE_PYTHONIST:
+            return FakePythonist(self.result)
+        elif self.result > self.NEWBIE_PYTHONIST:
+            return NewbiePythonist()
+        elif self.result > self.SENIOR_PYTHONIST:
+            return SeniorPythonist()
+        elif self.result > self.U_ARE_PYTHONIC:
+            return YouArePythonic()
+        elif self.result == self.GUIDO:
+            return HelloGuido()
+
+class FakePythonist(PEP8Achievement):
+    name = "Fake Pythonist"
+
+class NewbiePythonist(object):
+    pass
+
+class SeniorPythonist(object):
+    pass
+
+class YouArePythonic(object):
+    pass
+
+class HelloGuido(object):
+    pass
